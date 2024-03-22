ef00aec1f3b46a123b2dc0a0f953cfcb5035dc98
Niston <vadimpavlov@bk.ru>
2024-03-23 02:21:48 +0300

    main menu

---
+++ Assets/Scripts/UI/Buttons/ButtonManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void StartTheGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
